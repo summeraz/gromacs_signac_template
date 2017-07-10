ca76a22a393311b7dd1ba9d46306fe19c94de544
matt.thompson@vanderbilt.edu <matt.thompson@vanderbilt.edu>
2017-07-10 14:36:33 -0500

    Meta cleanups

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='GromacsSignacTemplate',
+    version='0.0.0',
+    author='Matthew W. Thompson',
+    license='LICENSE.txt',
+    description='Template for managing GROMACS projects with signac',
+)

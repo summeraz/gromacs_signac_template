2f2b9266e024026591b3a6ae617823dddc186444
mattwthompson <matt.thompson@vanderbilt.edu>
2018-02-09 10:07:56 -0600

    Remove check for block_avg import

--- src/anlyze.py
+++ src/anlyze.py
@@ -46,25 +46,19 @@ for job in project.find_jobs():
         t = data[:, 0]
         rho = data[:, 1]
 
-        if ba:
-            t_b, t_std = ba.block_avg(t, 50)
-            rho_b, rho_std = ba.block_avg(rho, 50)
-            t_b = t_b.reshape(-1)
-            t_std = t_std.reshape(-1)
-            rho_b = rho_b.reshape(-1)
-            rho_std = rho_std.reshape(-1)
-            myline, = ax.plot(t_b, 
-                rho_b,
-                marker='o',
-                markersize=5,
-                label='C_{}'.format(job.statepoint()['C_n']))
-            ax.fill_between(t_b, rho_b-rho_std, rho_b+rho_std, alpha=0.2, facecolor=myline.get_color())
-        else:
-            myline, = ax.plot(t, 
-                rho,
-                marker='o',
-                markersize=5,
-                label=job.statepoint()['C_n'])
+        t_b, t_std = ba.block_avg(t, 50)
+        rho_b, rho_std = ba.block_avg(rho, 50)
+        t_b = t_b.reshape(-1)
+        t_std = t_std.reshape(-1)
+        rho_b = rho_b.reshape(-1)
+        rho_std = rho_std.reshape(-1)
+        myline, = ax.plot(t_b, 
+            rho_b,
+            marker='o',
+            markersize=5,
+            label='C_{}'.format(job.statepoint()['C_n']))
+        ax.fill_between(t_b, rho_b-rho_std, rho_b+rho_std, alpha=0.2, facecolor=myline.get_color())
+
 ax.set_xlabel('Simulation time (ps)')
 ax.set_ylabel('Density (kg/m^3)')
 ax.legend(title='Alkane length', loc='lower right', fontsize='x-small')
